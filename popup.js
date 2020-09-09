62aa17195ff37f1aabae4e729fac6a8227fc51f3
Vipul Bhardwaj <33618798+vipulbhj@users.noreply.github.com>
2020-09-09 19:07:06 +0530

    Refactoring crappy js code

--- popup.js
+++ popup.js
@@ -1,34 +1,23 @@
 $(function() {
-    $("#button").click(function() {
-        var author = $("#author").val();
-        var quote = $("#quote").val();
-        if(!author) {
-            alert("Please provide a author");
-        }
-        else if(!quote) {
-            alert("Please provide a quote");
+  $("#button").click(function() {
+    var author = $("#author").val();
+    var quote = $("#quote").val();
+    if(!author) {
+      alert("Please provide a author");
+    }
+    else if(!quote) {
+      alert("Please provide a quote");
+    }
+    else {
+      chrome.storage.sync.get(null,function(items) {
+        var keys = Object.keys(items);
+        if(keys.include(author)) {
+          items[author].push(quote);
         }
         else {
-            alert(author);
-            chrome.storage.sync.get(null,function(items) {
-                var keys = Object.keys(items);
-                var found = false;
-                for(var i=0,l=keys.length;i<l;i++) {
-                    if(keys[i] === author) {
-                        found = true;
-                        break;
-                    }
-                }
-                if(found) {
-                    items[author].push(quote);
-                }
-                else {
-                    var ob = {};
-                    ob[author] = [quote];
-                    chrome.storage.sync.set(ob);
-                }
-                // console.log(items);
-            });
+          chrome.storage.sync.set({author: quote});
         }
-    });
+      });
+    }
+  });
 });
